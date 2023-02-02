d19cef618c4d8419a20516b75d95774de184a177
ibrahim tan <111127080+ibrahimtan315@users.noreply.github.com>
2023-02-02 14:47:13 +0300

    service

--- hr/src/main/java/com/fmss/hr/services/admin/SurveyService.java
+++ hr/src/main/java/com/fmss/hr/services/admin/SurveyService.java
@@ -1,6 +1,24 @@
 package com.fmss.hr.services.admin;
 
+import com.fmss.hr.dto.SurveyDto;
+import com.fmss.hr.entities.Survey;
+
+import java.util.List;
+
 public interface SurveyService {
+
+    Survey createSurvey(SurveyDto surveyDto);
+
+    void deleteSurveyById(Long surveyId);
+
+    Survey updateSurvey(SurveyDto surveyDto, Long id);
+
+    Survey getSurveyById(Long id);
+
+    List<Survey> getAllSurvey();
+
+    List<Survey> getAllSurveyWithStatus(Boolean isActive, int pageNum);
+
 
 
 }
--- hr/src/main/java/com/fmss/hr/services/admin/impl/SurveyServiceImpl.java
+++ hr/src/main/java/com/fmss/hr/services/admin/impl/SurveyServiceImpl.java
@@ -1,12 +1,47 @@
 package com.fmss.hr.services.admin.impl;
 
+import com.fmss.hr.dto.SurveyDto;
+import com.fmss.hr.entities.Survey;
+import com.fmss.hr.repos.admin.SurveyRepository;
 import com.fmss.hr.services.admin.SurveyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class SurveyServiceImpl implements SurveyService {
 
+    private final SurveyRepository surveyRepository;
 
+    @Override
+    public Survey createSurvey(SurveyDto surveyDto) {
+        return null;
+    }
+
+    @Override
+    public void deleteSurveyById(Long surveyId) {
+        surveyRepository.deleteById(surveyId);
+    }
+
+    @Override
+    public Survey updateSurvey(SurveyDto surveyDto, Long id) {
+        return null;
+    }
+
+    @Override
+    public Survey getSurveyById(Long id) {
+        return surveyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public List<Survey> getAllSurvey() {
+        return null;
+    }
+
+    @Override
+    public List<Survey> getAllSurveyWithStatus(Boolean isActive, int pageNum) {
+        return null;
+    }
 }
